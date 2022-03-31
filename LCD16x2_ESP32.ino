d130bac61e12752f1892877e66fcdd38e99b53b2
Rubén Lozano <70780174+RELL9030@users.noreply.github.com>
2022-03-31 11:35:05 -0500

    Update LCD16x2_ESP32.ino
    Hola mundo, conexión de módulo LCD 16x2 con Módulo ESP32

--- LCD16x2_ESP32.ino
+++ LCD16x2_ESP32.ino
@@ -9,14 +9,6 @@
 
 #include <LiquidCrystal.h>  
 
-/*
- * RS pin of LCD ? D# pin of NodeMCU or esp32
- * EN pin of LCD ? D# pin of NodeMCU or esp32
- * D4 pin of LCD ? D# pin of NodeMCU or esp32
- * D5 pin of LCD ? D# pin of NodeMCU or esp32
- * D6 pin of LCD ? D# pin of NodeMCU or esp32
- * D7 pin of LCD ? D# pin of NodeMCU or esp32
-*/
 #define RS 5 //PIN D19 o D5
 #define EN 17 //PIN D18 o TX2 GPIO17
 #define DB4 16 //PIN D5 o RX2 GPIO16
@@ -25,18 +17,16 @@
 #define DB7 15 //PIN D15
 
 
-LiquidCrystal lcd(RS, EN, DB4, DB5, DB6, DB7); //INICIALIZACION LCD 16x2
+LiquidCrystal lcd(RS, EN, DB4, DB5, DB6, DB7); //Pines para conexión LCD
 
 /*
  *****************************************************
- *
  *    SETUP
- *
  *****************************************************
 */
 void setup()
 {
-  Serial.begin(115200);      //Inicialización de la placa de desarrollo "ESP32"
+  Serial.begin(115200);      //Inicialización del bus serial
   lcd.begin(16, 2);         //Inicialización de la pantalla LCD 16x2.
   lcd.clear();              //Limpieza de pantalla
   lcd.setCursor(0, 0);      //Posición del cursor (F,C)
@@ -47,17 +37,15 @@ void setup()
 
 /*
  *****************************************************
- *
  *    LOOP
- *
  *****************************************************
  */
  
 void loop()
 {
-   // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
+   // Cambia el cursor a columna 0, línea 1
+  // (nota: la línea 1 es la segunda fila, ya que el conteo comienza con 0):
   lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
+  // imprime el número de segundos desde el reinicio:
   lcd.print(millis() / 1000);
 }
